31a00dcc80bad1d851bafdfdb9f6de0d9f2c41ef
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2019-10-24 19:31:35 +0300

    fixup! Добавляет PropTypes и props в App компонент

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 
 import {App} from './components/app/app.jsx';
 
+const mockFilms = [{
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+}, {
+  title: `Bohemian Rhapsody`,
+}, {
+  title: `Macbeth`,
+}, {
+  title: `Aviator`,
+}];
+
 const init = () => {
   ReactDOM.render(
-      <App/>,
+      <App
+        films={mockFilms}
+      />,
       document.getElementById(`root`)
   );
 };
