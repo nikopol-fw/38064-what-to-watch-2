eaea412b69111b503e032622a2c4dd67aea318b1
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2019-12-13 23:04:45 +0300

    Добавляет функционал для полноэкранного плеера

--- src/components/pages/player/player.tsx
+++ src/components/pages/player/player.tsx
@@ -5,13 +5,29 @@ import {RouteComponentProps} from 'react-router-dom';
 import {Film} from "../../../models/Film";
 import {getFilmById} from "../../../reducer/data/selectors";
 import {VideoPlayer} from "../../shared/video-player/video-player";
+import {formatTime} from "../../../lib/format-time/format-time";
 
 
 interface Props extends RouteComponentProps {
   film: Film;
 }
 
-export class Player extends React.PureComponent<Props> {
+interface Stats {
+  isPlaying: boolean;
+
+  percentage: number;
+  timeLeft: number;
+}
+
+export class Player extends React.PureComponent<Props, Stats> {
+  state = {
+    isPlaying: false,
+
+    percentage: 0,
+    timeLeft: 0,
+  };
+
+  private duration = 0;
 
   private readonly videoRef = React.createRef<HTMLVideoElement>();
 
@@ -41,6 +57,59 @@ export class Player extends React.PureComponent<Props> {
     super(props);
 
     this.clickExitBtnHandler = this.clickExitBtnHandler.bind(this);
+    this.clickPlayBtnHandler = this.clickPlayBtnHandler.bind(this);
+    this.clickPauseBtnHandler = this.clickPauseBtnHandler.bind(this);
+    this.clickFullScreenBtnHandler = this.clickFullScreenBtnHandler.bind(this);
+    this.loadedMetadataHandler = this.loadedMetadataHandler.bind(this);
+    this.updateTimeHandler = this.updateTimeHandler.bind(this);
+  }
+
+  componentDidUpdate() {
+    const videoElement = this.videoRef.current;
+    if (this.state.isPlaying) {
+      videoElement.play();
+    } else {
+      videoElement.pause();
+    }
+  }
+
+  clickPlayBtnHandler() {
+    this.setState({
+      isPlaying: true,
+    });
+  }
+
+  clickPauseBtnHandler() {
+    this.setState({
+      isPlaying: false,
+    });
+  }
+
+  clickFullScreenBtnHandler() {
+    const video = this.videoRef.current;
+    if (!video.requestFullscreen) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      if (video.mozRequestFullScreen) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        video.mozRequestFullScreen();
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+      } else if (video.webkitRequestFullScreen) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        video.webkitRequestFullScreen();
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+      } else if (video.msRequestFullscreen) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        video.msRequestFullscreen();
+      }
+    } else {
+      video.requestFullscreen();
+    }
   }
 
   clickExitBtnHandler() {
@@ -52,8 +121,26 @@ export class Player extends React.PureComponent<Props> {
     }
   }
 
+  loadedMetadataHandler() {
+    const video = this.videoRef.current;
+    this.duration = video.duration;
+    this.setState({
+      timeLeft: this.duration,
+    });
+  }
+
+  updateTimeHandler() {
+    const video = this.videoRef.current;
+    const timeLeft = this.duration - video.currentTime;
+    const percentage = video.currentTime / this.duration * 100;
+    this.setState({percentage, timeLeft});
+  }
+
   render() {
     const {film} = this.props;
+    const {percentage, timeLeft} = this.state;
+
+    const formattedTiming = formatTime(timeLeft, `seconds`, `HH:mm:ss`);
 
     return (
       <div className="player">
@@ -61,6 +148,8 @@ export class Player extends React.PureComponent<Props> {
           src={film.videoLink}
           poster={film.previewImage}
           ref={this.videoRef}
+          onLoadedMetadata={this.loadedMetadataHandler}
+          onTimeUpdate={this.updateTimeHandler}
         />
 
         <button type="button" className="player__exit" onClick={this.clickExitBtnHandler}>Exit</button>
@@ -68,22 +157,33 @@ export class Player extends React.PureComponent<Props> {
         <div className="player__controls">
           <div className="player__controls-row">
             <div className="player__time">
-              <progress className="player__progress" value="30" max="100"/>
-              <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
+              <progress className="player__progress" value={percentage} max="100"/>
+              <div className="player__toggler" style={{left: `${percentage}%`}}>Toggler</div>
             </div>
-            <div className="player__time-value">1:30:29</div>
+            <div className="player__time-value">{formattedTiming}</div>
           </div>
 
           <div className="player__controls-row">
-            <button type="button" className="player__play">
-              <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref="#play-s"/>
-              </svg>
-              <span>Play</span>
-            </button>
-            <div className="player__name">Transpotting</div>
+            {this.state.isPlaying
+              ? (
+                <button type="button" className="player__play" onClick={this.clickPauseBtnHandler}>
+                  <svg viewBox="0 0 14 21" width="14" height="21">
+                    <use xlinkHref="#pause"/>
+                  </svg>
+                  <span>Pause</span>
+                </button>
+              ) : (
+                <button type="button" className="player__play" onClick={this.clickPlayBtnHandler}>
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"/>
+                  </svg>
+                  <span>Play</span>
+                </button>
+              )
+            }
+            <div className="player__name">{film.name}</div>
 
-            <button type="button" className="player__full-screen">
+            <button type="button" className="player__full-screen" onClick={this.clickFullScreenBtnHandler}>
               <svg viewBox="0 0 27 27" width="27" height="27">
                 <use xlinkHref="#full-screen"/>
               </svg>
---
+++ src/lib/format-time/format-time.ts
@@ -0,0 +1,7 @@
+import * as moment from 'moment';
+import 'moment-duration-format';
+
+
+export const formatTime = (time: number, unit: `seconds`, format: string): string => {
+  return moment.duration(time, unit).format(format, {trim: false});
+};
